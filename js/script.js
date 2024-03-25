32d8abcda3d3fc44e970e82f9fa383a66f001775
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-03-25 15:29:58 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+window.onload = function(){
+    alert("본 페이지는 1920px에 최적화 되어있습니다.")
 
-let input = [];
 
-rl.on('line', function (line) {
-    input = [line];
-}).on('close',function(){
-    str = input[0];
-     
-    console.log(str)
-});
+    const modalBox = document.querySelector(".modal-box > span")
+    const modalImg = document.querySelector(".modal-img")
+    const close = document.querySelector(".close")
+    const bodyBack = document.querySelector("body")
+
+    modalBox.addEventListener('click',function(){
+        modalImg.style.display = 'block';
+    });
+    close.addEventListener('click',function(){
+        modalImg.style.display = 'none';
+    });
+}
